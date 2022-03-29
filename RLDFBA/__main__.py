1b4c70ddf57cb8c3a1a9a66cd4e07ba626a22f6f
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2022-03-29 12:18:37 -0600

    DFBA with policy ready

--- RLDFBA/__main__.py
+++ RLDFBA/__main__.py
@@ -8,10 +8,12 @@ import os
 from scipy.integrate import solve_ivp
 from scipy.integrate import odeint
 import random
+import matplotlib.pyplot as plt
 
 Main_dir = os.path.dirname(os.path.abspath(__file__))
 
-def main(Number_of_Models: int = 2, max_time: int = 100, Dil_Rate: float = 0.1):
+
+def main(Number_of_Models: int = 2, max_time: int = 100, Dil_Rate: float = 0.000001):
     """
     This is the main function for running dFBA.
     The main requrement for working properly is
@@ -21,52 +23,57 @@ def main(Number_of_Models: int = 2, max_time: int = 100, Dil_Rate: float = 0.1):
 
     """
 
-    Models = []  
+    Models = []
     Base_Model = cobra.io.read_sbml_model(
         os.path.join(Main_dir, 'IJO1366_AP.xml'))
     [Models.append(Base_Model.copy()) for i in range(Number_of_Models)]
 
     for i in range(Number_of_Models):
         Models[i].name = "Ecoli_"+str(i+1)
-        Models[i].solver.objective.name="_pfba_objective"
+        Models[i].solver.objective.name = "_pfba_objective"
 
     Mapping_Dict = Build_Mapping_Matrix(Models)
-    Init_C = np.zeros((Models.__len__()+Mapping_Dict["Ex_sp"].__len__()+1,))
+    Init_C = np.ones((Models.__len__()+Mapping_Dict["Ex_sp"].__len__()+1,))
     Inlet_C = np.zeros((Models.__len__()+Mapping_Dict["Ex_sp"].__len__()+1,))
 
-    ##The Params are the main part to change from problem to problem
+    # The Params are the main part to change from problem to problem
 
     Params = {
         "Dilution_Rate": Dil_Rate,
-        "Glucose_Index": Mapping_Dict["Ex_sp"].index("EX_glc__D(e)")+Models.__len__()+Models.__len__(),
+        "Glucose_Index": Mapping_Dict["Ex_sp"].index("EX_glc__D(e)")+Models.__len__(),
         "Starch_Index": Mapping_Dict["Ex_sp"].__len__()+Models.__len__(),
         "Amylase_Ind": Mapping_Dict["Ex_sp"].index("EX_amylase(e)")+Models.__len__(),
         "Inlet_C": Inlet_C,
         "Model_Glc_Conc_Index": [Models[i].reactions.index("EX_glc__D(e)") for i in range(Number_of_Models)],
         "Model_Amylase_Conc_Index": [Models[i].reactions.index("EX_amylase(e)") for i in range(Number_of_Models)],
-        "Num_Glucose_States":10,
-        "Num_Starch_States":10,
-        "Num_Amylase_States":10
+        "Num_Glucose_States": 10,
+        "Num_Starch_States": 10,
+        "Num_Amylase_States": 10
 
     }
 
-    Init_C[[Params["Glucose_Index"],Params["Starch_Index"],Params["Amylase_Ind"]]] = 1
-
-    ### Policy initialization
-    ### Initial Policy is set to a random policy
-    Init_Policy_Dict={}
-    States=[(i,j,k) for i in range(Params["Num_Glucose_States"]) for j in range(Params["Num_Starch_States"]) for k in range(Params["Num_Amylase_States"])]
-    for state in States:
-        Init_Policy_Dict[state]=random.choice(range(Params["Num_Amylase_States"]))
-    
+    Init_C[[Params["Glucose_Index"],
+            Params["Starch_Index"], Params["Amylase_Ind"]]] = 100
     for i in range(Number_of_Models):
-        Models[i].Policy=Policy_Deterministic(Init_Policy_Dict)
+        Init_C[i] = 0.001
 
+    # Policy initialization
+    # Initial Policy is set to a random policy
+    Init_Policy_Dict = {}
+    States = [(i, j, k) for i in range(Params["Num_Glucose_States"]) for j in range(
+        Params["Num_Starch_States"]) for k in range(Params["Num_Amylase_States"])]
+    for state in States:
+        Init_Policy_Dict[state] = random.choice(range(9))
 
+    for i in range(Number_of_Models):
+        Models[i].Policy = Policy_Deterministic(Init_Policy_Dict)
 
-
-
-    Conc=dFBA(Models, Mapping_Dict, Init_C, Params)
+    Conc, t = dFBA(Models, Mapping_Dict, Init_C, Params)
+    for i in range(Number_of_Models):
+        plt.plot(t, Conc[:, i])
+    for species in ["Glucose_Index", "Starch_Index", "Amylase_Ind"]:
+        plt.plot(t, Conc[:, Params[species]])
+    plt.show()
     print(Conc)
 
     ############################
@@ -74,7 +81,7 @@ def main(Number_of_Models: int = 2, max_time: int = 100, Dil_Rate: float = 0.1):
     ############################
 
 
-def dFBA(Models, Mapping_Dict, Init_C,Params):
+def dFBA(Models, Mapping_Dict, Init_C, Params):
     """
     This function calculates the concentration of each species
     Models is a list of COBRA Model objects
@@ -87,14 +94,13 @@ def dFBA(Models, Mapping_Dict, Init_C,Params):
     ##############################################################
     # Solving the ODE
     ##############################################################
-    sol = odeint(ODE_System, Init_C,range(100), args=(Models, Mapping_Dict, Params),rtol=100000)
-    C = sol.y
-    return C
+
+    sol, t = odeFwdEuler(ODE_System, Init_C, 0.1,  Params,
+                         [0, 100], Models, Mapping_Dict)
+    return sol, t
 
 
-
-def ODE_System(C,t , Models, Mapping_Dict, Params):
-
+def ODE_System(C, t, Models, Mapping_Dict, Params):
     """
     This function calculates the differential equations for the system
     Models is a list of COBRA Model objects
@@ -112,35 +118,41 @@ def ODE_System(C,t , Models, Mapping_Dict, Params):
     [n+m]-Starch
     """
     dCdt = np.zeros(C.shape)
+    Sols = [0]*Models.__len__()
 
     for i in range(Models.__len__()):
-        Models[i].State = Descretize_Concs(C[Params["Glucose_Index"]], C[Params["Starch_Index"]], C[Params["Amylase_Ind"]])
+        Models[i].State = Descretize_Concs(
+            C[Params["Glucose_Index"]], C[Params["Starch_Index"]], C[Params["Amylase_Ind"]])
 
     for j in range(Mapping_Dict["Mapping_Matrix"].shape[0]):
         for i in range(Models.__len__()):
             if Mapping_Dict["Mapping_Matrix"][j, i] != Params["Model_Glc_Conc_Index"][i]:
-                if Mapping_Dict["Mapping_Matrix"][j, i] !=Params["Model_Amylase_Conc_Index"][i]:
-                    Models[i].reactions[Mapping_Dict["Mapping_Matrix"][j, i]
-                                    ].upper_bound = General_Uptake_Kinetics(C[j+Models.__len__()-1])
+                if Mapping_Dict["Mapping_Matrix"][j, i] != Params["Model_Amylase_Conc_Index"][i]:
+                    pass
+                    # Models[i].reactions[Mapping_Dict["Mapping_Matrix"][j, i]
+                    #                     ].lower_bound = - General_Uptake_Kinetics(C[j+Models.__len__()])
             else:
                 Models[i].reactions[Mapping_Dict["Mapping_Matrix"][j, i]
-                                    ].upper_bound = Glucose_Uptake_Kinetics(C[j])
-            Models[i].reactions[Params["Model_Amylase_Conc_Index"]
-                                [i]].lower_bound = (lambda x, a: a*x/100)(Models[i].Policy.get_action(Models[i].State), 5)
+                                    ].lower_bound = - Glucose_Uptake_Kinetics(C[j+Models.__len__()])
+    for i in range(Models.__len__()):
+        Models[i].reactions[Params["Model_Amylase_Conc_Index"]
+                            [i]].lower_bound = (lambda x, a: a*x/100)(Models[i].Policy.get_action(Models[i].State), 5)
 
     for i in range(Models.__len__()):
-        Models[i].optimize()
-        dCdt[i] += Models[i].objective.value*C[i]
+        Sols[i] = Models[i].optimize()
+        dCdt[i] += Sols[i].objective_value*C[i]
 
     for i in range(Mapping_Dict["Mapping_Matrix"].shape[0]):
         for j in range(Models.__len__()):
             if Mapping_Dict["Mapping_Matrix"][i, j] != -1:
-                dCdt[i+Models.__len__()] += Models[j].reactions[Mapping_Dict["Mapping_Matrix"]
-                                      [i, j]].x*C[i+Models.__len__()]
+                dCdt[i+Models.__len__()] += Sols[j].fluxes.iloc[Mapping_Dict["Mapping_Matrix"]
+                                                                [i, j]]*C[j]
 
-    dCdt[Params["Starch_Index"]] =- Starch_Degradation_Kinetics(C[Params["Amylase_Ind"]],C[Params["Starch_Index"]])
+    dCdt[Params["Starch_Index"]] = - \
+        Starch_Degradation_Kinetics(
+            C[Params["Amylase_Ind"]], C[Params["Starch_Index"]])
 
-    dCdt+=np.array(Params["Dilution_Rate"])*(Params["Inlet_C"]-C)
+    dCdt += np.array(Params["Dilution_Rate"])*(Params["Inlet_C"]-C)
     print(t)
     return dCdt
 
@@ -159,26 +171,25 @@ def Build_Mapping_Matrix(Models):
 
     """
 
-    Ex_sp=[]
+    Ex_sp = []
     for model in Models:
         for Ex_rxn in model.exchanges:
             if Ex_rxn.id not in Ex_sp:
                 Ex_sp.append(Ex_rxn.id)
-    Mapping_Matrix=np.zeros((len(Ex_sp), len(Models)), dtype=int)
+    Mapping_Matrix = np.zeros((len(Ex_sp), len(Models)), dtype=int)
     for i, id in enumerate(Ex_sp):
         for j, model in enumerate(Models):
             if id in model.reactions:
-                Mapping_Matrix[i, j]=model.reactions.index(id)
+                Mapping_Matrix[i, j] = model.reactions.index(id)
             else:
-                Mapping_Matrix[i, j]=-1
+                Mapping_Matrix[i, j] = -1
     return {"Ex_sp": Ex_sp, "Mapping_Matrix": Mapping_Matrix, "Models": Models}
-
-
 
 
 class Policy_Deterministic:
     def __init__(self, Policy):
-        self.Policy=Policy
+        self.Policy = Policy
+
     def get_action(self, state):
         return self.Policy[state]
 
@@ -191,22 +202,22 @@ class Policy_General:
     """
 
     def __init__(self, Porb_Dist_Dict):
-        self.Policy=Porb_Dist_Dict
+        self.Policy = Porb_Dist_Dict
 
     def get_action(self, state):
-        Actions=[(action, self.Policy[state][action])
+        Actions = [(action, self.Policy[state][action])
                    for action in self.Policy[state].keys()]
         np.random.choice(Actions, p=[action[1] for action in Actions], k=1)
 
 
-def Starch_Degradation_Kinetics(a_Amylase: float, Starch: float, Model="",k: float=0.1):
+def Starch_Degradation_Kinetics(a_Amylase: float, Starch: float, Model="", k: float = 0.1):
     """
     This function calculates the rate of degradation of starch
     a_Amylase Unit: mmol
     Starch Unit: mg
 
     """
-    
+
     return a_Amylase*Starch*k
 
 
@@ -228,25 +239,31 @@ def General_Uptake_Kinetics(Compound: float, Model=""):
 
     """
     return 100*(Compound/(Compound+20))
+
+
 def Descretize_Concs(Glucose: float, Starch: float, Amylase: float):
     """
     This function calculates the state of the reactor
 
 
     """
-    	
-    Glucose=np.digitize(Glucose,range(9))
-    Starch=np.digitize(Starch,range(9))
-    Amylase=np.digitize(Amylase,range(9))
+
+    Glucose = np.digitize(Glucose, range(9))
+    Starch = np.digitize(Starch, range(9))
+    Amylase = np.digitize(Amylase, range(9))
 
     return (Glucose, Starch, Amylase)
 
 
-def odeFwdEuler(ODE_Function, ICs,dt, Params, t_span):
-    
-    #My Own ODE Solver
-    pass
+def odeFwdEuler(ODE_Function, ICs, dt, Params, t_span, Models, Mapping_Dict):
 
+    t = np.arange(t_span[0], t_span[1], dt)
+    sol = np.zeros((len(t), len(ICs)))
+    sol[0] = ICs
+    for i in range(1, len(t)):
+        sol[i] = sol[i-1] + \
+            ODE_Function(sol[i-1], t[i-1], Models, Mapping_Dict, Params)*dt
+    return sol, t
 
 
 if __name__ == "__main__":
