342b7d9316abf95cb7acea608e85f1bfa838d5f0
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2022-07-12 01:00:19 -0600

    NECOM Toys

---
+++ NECOM/ToyModel.py
@@ -0,0 +1,192 @@
+from cobra import Model, Reaction, Metabolite
+
+"""
+Toy Model 1:
+
+
+EX_S_sp1: S -> lowerBound',-10,'upperBound',0
+EX_A_sp1: A -> lowerBound',-100,'upperBound',100
+EX_B_sp1: B -> lowerBound',-100,'upperBound',100
+EX_P_sp1: P->  lowerBound',0,'upperBound',100
+R_1_sp1: S  + 2 adp  -> P + 2 atp ,'lowerBound',0,'upperBound',Inf
+R_2_sp1: P + atp  -> B  + adp 'lowerBound',0,'upperBound',Inf
+R_3_sp1: P + 3 atp  -> A + 3 adp ,'lowerBound',0,'upperBound',Inf
+R_4_sp1: 'atp -> adp  lowerBound',0,'upperBound',Inf
+OBJ_sp1: 3 A + 3 B + 5 atp  -> 5 adp + biomass_sp1 lowerBound',0,'upperBound',Inf
+Biomass_1 biomass_sp1  -> ','lowerBound',0,'upperBound',Inf,'objectiveCoef', 1);
+
+"""
+Toy1 = Model('Toy_1')
+
+### S_Uptake ###
+
+EX_S_sp1 = Reaction('EX_S_sp1')
+S = Metabolite('S', compartment='c')
+EX_S_sp1.add_metabolites({S: -1})
+EX_S_sp1.lower_bound = -10
+EX_S_sp1.upper_bound = 0
+Toy1.add_reaction(EX_S_sp1)
+
+
+EX_A_sp1 = Reaction('EX_A_sp1')
+A = Metabolite('A', compartment='c')
+EX_A_sp1.add_metabolites({A: -1})
+EX_A_sp1.lower_bound = -100
+EX_A_sp1.upper_bound = 100
+Toy1.add_reaction(EX_A_sp1)
+
+
+EX_B_sp1 = Reaction('EX_B_sp1')
+B = Metabolite('B', compartment='c')
+EX_B_sp1.add_metabolites({B: -1})
+EX_B_sp1.lower_bound = -100
+EX_B_sp1.upper_bound = 100
+Toy1.add_reaction(EX_B_sp1)
+
+
+
+EX_P_sp1 = Reaction('EX_P_sp1')
+P = Metabolite('P', compartment='c')
+EX_P_sp1.add_metabolites({P:-1})
+EX_P_sp1.lower_bound = 0
+EX_P_sp1.upper_bound = 100
+Toy1.add_reaction(EX_P_sp1)
+
+
+R_1_sp1 = Reaction('R_1_sp1')
+ADP = Metabolite('ADP', compartment='c')
+ATP = Metabolite('ATP', compartment='c')
+R_1_sp1.add_metabolites({ADP: -2, S: -1, P: 1, ATP: 2})
+R_1_sp1.lower_bound = 0
+R_1_sp1.upper_bound = 1000
+Toy1.add_reaction(R_1_sp1)
+
+
+R_2_sp1 = Reaction('R_2_sp1')
+R_2_sp1.add_metabolites({ADP: 1, P: -1, B: 1, ATP: -1})
+R_2_sp1.lower_bound = 0
+R_2_sp1.upper_bound = 1000
+Toy1.add_reaction(R_2_sp1)
+
+
+R_3_sp1 = Reaction('R_3_sp1')
+R_3_sp1.add_metabolites({ADP: 3, P: -1, A: 1, ATP: -3})
+R_3_sp1.lower_bound = 0
+R_3_sp1.upper_bound = 1000
+Toy1.add_reaction(R_3_sp1)
+
+
+
+R_4_sp1 = Reaction('R_4_sp1')
+R_4_sp1.add_metabolites({ADP:1 ,ATP: -1})
+R_4_sp1.lower_bound = 0
+R_4_sp1.upper_bound = 1000
+Toy1.add_reaction(R_4_sp1)
+
+
+
+
+OBJ_sp1 = Reaction("OBJ_sp1")
+biomass_sp1 = Metabolite('biomass_sp1', compartment='c')
+OBJ_sp1.add_metabolites({ADP:5 ,ATP: -5,biomass_sp1:1,A:-3,B:-3})
+OBJ_sp1.lower_bound = 0
+OBJ_sp1.upper_bound = 1000
+Toy1.add_reaction(OBJ_sp1)
+
+Biomass_1 = Reaction("Biomass_1")
+Biomass_1.add_metabolites({biomass_sp1:-1})
+Biomass_1.lower_bound = 0
+Biomass_1.upper_bound = 1000
+Toy1.add_reaction(Biomass_1)
+
+
+
+### ADP Production From Catabolism ###
+
+Toy2 = Model('Toy_2')
+
+### S_Uptake ###
+
+EX_S_sp2 = Reaction('EX_S_sp2')
+S = Metabolite('S', compartment='c')
+EX_S_sp2.add_metabolites({S: -1})
+EX_S_sp2.lower_bound = -10
+EX_S_sp2.upper_bound = 0
+Toy1.add_reaction(EX_S_sp2)
+
+
+EX_A_sp2 = Reaction('EX_A_sp2')
+A = Metabolite('A', compartment='c')
+EX_A_sp2.add_metabolites({A: -1})
+EX_A_sp2.lower_bound = -100
+EX_A_sp2.upper_bound = 100
+Toy2.add_reaction(EX_A_sp2)
+
+
+EX_B_sp2 = Reaction('EX_B_sp2')
+B = Metabolite('B', compartment='c')
+EX_B_sp2.add_metabolites({B: -1})
+EX_B_sp2.lower_bound = -100
+EX_B_sp2.upper_bound = 100
+Toy1.add_reaction(EX_B_sp2)
+
+
+
+EX_P_sp2 = Reaction('EX_P_sp2')
+P = Metabolite('P', compartment='c')
+EX_P_sp2.add_metabolites({P:-1})
+EX_P_sp2.lower_bound = 0
+EX_P_sp2.upper_bound = 100
+Toy2.add_reaction(EX_P_sp2)
+
+
+R_1_sp2 = Reaction('R_1_sp2')
+ADP = Metabolite('ADP', compartment='c')
+ATP = Metabolite('ATP', compartment='c')
+R_1_sp2.add_metabolites({ADP: -3, S: -1, P: 1, ATP: 3})
+R_1_sp2.lower_bound = 0
+R_1_sp2.upper_bound = 1000
+Toy2.add_reaction(R_1_sp2)
+
+
+R_2_sp2 = Reaction('R_2_sp2')
+R_2_sp2.add_metabolites({ADP: 3, P: -1, B: 1, ATP: -3})
+R_2_sp2.lower_bound = 0
+R_2_sp2.upper_bound = 1000
+Toy2.add_reaction(R_2_sp2)
+
+
+R_3_sp2 = Reaction('R_3_sp2')
+R_3_sp2.add_metabolites({ADP: 1, P: -1, A: 1, ATP: -1})
+R_3_sp2.lower_bound = 0
+R_3_sp2.upper_bound = 1000
+Toy2.add_reaction(R_3_sp2)
+
+
+
+R_4_sp2 = Reaction('R_4_sp2')
+R_4_sp2.add_metabolites({ADP:1 ,ATP: -1})
+R_4_sp2.lower_bound = 0
+R_4_sp2.upper_bound = 1000
+Toy2.add_reaction(R_4_sp2)
+
+
+
+
+OBJ_sp2 = Reaction("OBJ_sp2")
+biomass_sp2 = Metabolite('biomass_sp2', compartment='c')
+OBJ_sp2.add_metabolites({ADP:5 ,ATP: -5,biomass_sp2:1,A:-6,B:-6})
+OBJ_sp2.lower_bound = 0
+OBJ_sp2.upper_bound = 1000
+Toy2.add_reaction(OBJ_sp2)
+
+Biomass_2 = Reaction("Biomass_2")
+Biomass_2.add_metabolites({biomass_sp2:-1})
+Biomass_2.lower_bound = 0
+Biomass_2.upper_bound = 1000
+Toy2.add_reaction(Biomass_2)
+
+
+if __name__ == '__main__':
+    print(Toy1.optimize().fluxes)
+    print(Toy1.exchanges)
