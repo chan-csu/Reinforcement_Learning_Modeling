6dc632e41c5c65d47f9139bed22a5b0072d34cd8
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2022-08-24 23:23:05 -0600

    Update NECOM_System.py

--- Actor_Critic/NECOM_System.py
+++ Actor_Critic/NECOM_System.py
@@ -27,11 +27,10 @@ import warnings
 import torch.autograd
 from torch.autograd import Variable
 import gym
-
 warnings.filterwarnings("ignore")
 Scaler=StandardScaler()
 HIDDEN_SIZE=20
-NUMBER_OF_BATCHES=100
+NUMBER_OF_BATCHES=1000
 Main_dir = os.path.dirname(os.path.abspath(__file__))
 
 Episode = namedtuple('Episode', field_names=['reward', 'steps'])
@@ -177,14 +176,14 @@ def main(Models: list = [Toy_Model_NE_1.copy(), Toy_Model_NE_2.copy()], max_time
         m.value=DDPGCritic(len(m.observables),len(m.actions))
         m.value_target=DDPGCritic(len(m.observables),len(m.actions))
         m.R=0
-        m.tau=0.001
+        m.tau=0.0001
         m.optimizer_policy=optim.Adam(params=m.policy.parameters(), lr=0.01)
         m.optimizer_policy_target=optim.Adam(params=m.policy.parameters(), lr=0.01)
         m.optimizer_value=optim.Adam(params=m.value.parameters(), lr=0.01)
         m.optimizer_value_target=optim.Adam(params=m.value.parameters(), lr=0.01)
         m.Net_Obj=nn.MSELoss()
         m.epsilon=0.1
-        m.buffer=Memory(1000)
+        m.buffer=Memory(10000)
         m.alpha=0.01
         m.update_batch=200
         
@@ -327,7 +326,7 @@ def ODE_System(C, t, Models, Mapping_Dict, Params, dt,Counter):
         m.buffer.push(torch.FloatTensor([C[m.observables]]).detach().numpy()[0],m.a,m.reward,torch.FloatTensor([Next_C[m.observables]]).detach().numpy()[0])
         if Counter>0 and Counter%m.update_batch==0:
             # TD_Error=[]
-            S,A,R,Sp=m.buffer.sample(30)
+            S,A,R,Sp=m.buffer.sample(100)
             Qvals = m.value(torch.FloatTensor(S), torch.FloatTensor(A))
             next_actions = m.policy(torch.FloatTensor(Sp))
             next_Q = m.value_target.forward(torch.FloatTensor(Sp), next_actions.detach())
@@ -432,7 +431,7 @@ def General_Uptake_Kinetics(Compound: float, Model=""):
     Compound Unit: mmol
 
     """
-    return 30*(Compound/(Compound+20))
+    return 100*(Compound/(Compound+20))
 
 
 
@@ -458,6 +457,8 @@ def Generate_Batch(dFBA, Params, Init_C, Models, Mapping_Dict,t_span=[0, 100], d
 
     
     for BATCH in range(NUMBER_OF_BATCHES):
+        for model in Models:
+            model.epsilon=0.01+0.99/(np.exp(BATCH/20))
         dFBA(Models, Mapping_Dict, Init_C, Params, t_span, dt=dt)
     
         for mod in Models:
