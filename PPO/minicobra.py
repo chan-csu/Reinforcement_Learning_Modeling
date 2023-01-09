7e509b2343e7ab96c07445b156384bc932f47890
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2023-01-08 21:33:55 -0700

    functional minicobra

--- PPO/minicobra.py
+++ PPO/minicobra.py
@@ -1,5 +1,8 @@
 import cobra
 import numpy as np
+from functools import cached_property
+from gurobipy import Model as gurobiModel
+from gurobipy import GRB
 
 class Model:
     """A simple model similar to cobra model that is supposed to 
@@ -10,37 +13,67 @@ class Model:
     def __init__(self,reactions:list[cobra.Reaction],metabolites:list[cobra.Metabolite],objective=str):
         self.reactions = reactions
         self.metabolites = metabolites
-        self.objective = self.index_objective(objective)
-        self.update_stoichiometric_matrix()
+        self._objective=objective
+        self.reactionids=   [reaction.id for reaction in self.reactions]
+        self.metaboliteids= [metabolite.id for metabolite in self.metabolites]
+        self.empty_gb_model=gurobiModel()
 
 
     def remove_reactions(self,reactions):
         """Remove reactions from the model"""
         for reaction in reactions:
             self.reactions.remove(reaction)
+        self.update_stoichiometric_matrix()
 
     
     def add_reactions(self,reactions):
         """Add reactions to the model"""
         for reaction in reactions:
             self.reactions.append(reaction)
-
-    def update_stoichiometric_matrix(self):
+    
+    @cached_property
+    def s(self):
         """Update the stoichiometric matrix based on metabolites and reactions"""
-        s=np.zeros((len(self.reactions),len(self.metabolites)))
+        s=np.zeros((len(self.metabolites),len(self.reactions)))
         for i,reaction in enumerate(self.reactions):
             for metabolite in reaction.metabolites:
-                s[i,self.metabolites.index(metabolite)]=reaction.metabolites[metabolite]
-        self.s=s
+                s[self.metabolites.index(metabolite),i]=reaction.metabolites[metabolite]
+        return s
 
-    def index_objective(self,objective):
-        """finds the index of the objective function"""
-        return self.reactions.index(objective)
+    @property
+    def objective(self):
+        return self.reactions.index(self._objective)
+
+    @objective.setter
+    def objective(self,value):
+        self._objective=value
+    
+    @property
+    def lb(self):
+        return np.array([reaction.lower_bound for reaction in self.reactions])
+    
+    @property
+    def ub(self):
+        return np.array([reaction.upper_bound for reaction in self.reactions])
+
+    def optimize(self):
+        """Optimize the model"""
+        model=self.empty_gb_model.copy()
+        model.setParam('OutputFlag',False)
+        x=model.addMVar(shape=self.s.shape[1],lb=self.lb,ub=self.ub,vtype=GRB.CONTINUOUS,name="x")
+        b=np.zeros((self.s.shape[0],))
+        model.addMConstr(self.s,x,"=",b)
+        model.setObjective(x[self.objective],GRB.MAXIMIZE)
+        model.optimize()
+        return model
+
+    
+
+
 
             
 
 if __name__=='__main__':
     model=cobra.io.read_sbml_model("iAF1260.xml")
-    mini_model=Model(model.reactions,model.metabolites)
-    mini_model.update_stoichiometric_matrix()
-    mini_model.s
+    mini_model=Model(model.reactions,model.metabolites,"BIOMASS_Ec_iAF1260_core_59p81M")
+    sol=mini_model.optimize()
